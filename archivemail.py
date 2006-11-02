faa68b20487f2d1f48a43e11adff4d02689be418
Nikolaus Schulz <microschulz@web.de>
2006-11-02 06:43:09 +0000

    Bumped version to 0.7.0

--- archivemail.py
+++ archivemail.py
@@ -24,7 +24,7 @@ Website: http://archivemail.sourceforge.net/
 """
 
 # global administrivia 
-__version__ = "archivemail v0.6.2"
+__version__ = "archivemail v0.7.0"
 __svn_id__ = "$Id$"
 __copyright__ = """\
 Copyright (C) 2002  Paul Rodger <paul@paulrodger.com>
