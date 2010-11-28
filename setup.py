393f08bb2edd764c2810b2b88e6eef80952f44bd
Nikolaus Schulz <microschulz@web.de>
2010-11-28 02:02:51 +0100

    setup.py: install manpage into share/man/man1 instead of man/man1
    This change aims to comply with the FHS, which puts manpages into
    /usr/share/man, not /usr/man.

--- setup.py
+++ setup.py
@@ -29,5 +29,5 @@ setup(name="archivemail",
       maintainer="Nikolaus Schulz, Peter Poeml",
       maintainer_email="nikosch@users.sourceforge.net, poeml@users.sourceforge.net",
       scripts=["archivemail"],
-      data_files=[("man/man1", ["archivemail.1"])],
+      data_files=[("share/man/man1", ["archivemail.1"])],
       )
